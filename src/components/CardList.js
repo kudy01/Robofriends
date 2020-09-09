6a71abf8ebbde050619eac883a16f5bc62af0fe0
Kushal Jaiswal <68225773+kudy01@users.noreply.github.com>
2020-09-09 20:37:49 +0530

    project added

---
+++ src/components/CardList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({robots}) => {
+	return (
+		<div>
+				{
+				robots.map((user,i) => {
+					return(
+						<Card
+						key ={i}
+						id={robots[i].id} 
+						name={robots[i].name}
+						username={robots[i].username} 
+						email={robots[i].email}
+						/>
+					);
+				})
+				}
+		</div> 
+	);
+}
+
+
+
+
+export default CardList;
