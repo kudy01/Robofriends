60cdfd32b42a35c1073857710501547ca15cbb50
Kushal Jaiswal <68225773+kudy01@users.noreply.github.com>
2020-09-11 19:23:53 +0530

    Error Handling Added

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+class ErrorBoundary extends Component{
+	
+	constructor(props){
+		super(props);
+		this.state = {
+			hasError: false
+		}
+
+	}
+
+componentDidCatch(error, info) {
+	this.setState({hasError: true})
+}
+
+render(){
+	if (this.state.hasError){
+		return <h1>Oops. You made an error</h1> 
+	}
+	return this.props.children
+}
+
+}
+
+export default ErrorBoundary;
