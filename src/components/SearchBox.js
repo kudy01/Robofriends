bc4d1c82dd0f1d9ba1bdd54fb7f5b20f78fca721
Kushal Jaiswal <68225773+kudy01@users.noreply.github.com>
2020-10-14 18:59:56 +0530

    Increasing performance

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,9 +3,11 @@ import React from 'react';
 const SearchBox =({ searchChange }) => {
 	return (
 		<div className='pa2'>
-			<input className='pa3 ba b--green bg-lightest-blue'
+			<input 
+			aria-label='Search Robots'
+			className='pa3 ba b--green bg-lightest-blue'
 			type='search' 
-			placeholder='search robots' 
+			placeholder='Search Robots' 
 			onChange={searchChange}
 			/>
 		</div>	
